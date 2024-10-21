47c7ec4638ad154e5c37c051566684b6ad2ca120
Václav Vančura <commit@vancura.dev>
2024-10-21 15:32:57 +0200

    Adding the Walsheim font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {}
+        colors: {},
+        extend: {
+            fontFamily: {
+                walsheim: ['Walsheim Regular', 'sans-serif']
+            }
+        }
     },
     plugins: []
 };
