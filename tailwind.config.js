82d3dc64c97343a3bc3eaca9996dfbbdda0f8531
Václav Vančura <commit@vancura.dev>
2024-10-21 16:32:52 +0200

    Adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ const plugin = require('tailwindcss/plugin');
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        colors: {},
         extend: {
+            colors: {
+                'page-bg': {
+                    DEFAULT: '#F5F5F5',
+                    dark: '#333333'
+                },
+                'page-text': {
+                    DEFAULT: '#333333',
+                    dark: '#F5F5F5'
+                }
+            },
             fontFamily: {
                 walsheim: ['Walsheim Regular', 'sans-serif']
             }
