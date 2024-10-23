08d91af89b11b75d1f83676c0b111de3e1f340b7
Václav Vančura <commit@vancura.dev>
2024-10-23 11:27:57 +0200

    Tweaking Prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,10 @@
 module.exports = {
-    plugins: [
-        require.resolve('prettier-plugin-astro'),
-        require.resolve('prettier-plugin-organize-imports'),
-        require.resolve('prettier-plugin-organize-attributes'),
-        require.resolve('prettier-plugin-astro-organize-imports'),
-        require.resolve('prettier-plugin-css-order')
-    ],
+    plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-organize-imports'), require.resolve('prettier-plugin-organize-attributes'), require.resolve('prettier-plugin-astro-organize-imports'), require.resolve('prettier-plugin-css-order')],
 
     tabWidth: 4,
     useTabs: false,
-    printWidth: 120,
-    proseWrap: 'always',
+    printWidth: 1000,
+    proseWrap: 'never',
     singleQuote: true,
     semi: true,
     trailingComma: 'none',
--- strings.mjs
+++ strings.mjs
@@ -1,5 +1,4 @@
 export default {
     title: 'Actor Programming Model: Build Serverless Microapps',
-    description:
-        'Discover the Actor model for serverless microapps. Learn to develop, share, and monetize reusable tools for web scraping, automation, and AI agents.'
+    description: 'Discover the Actor model for serverless microapps. Learn to develop, share, and monetize reusable tools for web scraping, automation, and AI agents.'
 };
