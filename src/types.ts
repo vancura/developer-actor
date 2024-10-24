720786223b8af361bcba4ec12e9a4c2ea3c7521c
Václav Vančura <commit@vancura.dev>
2024-10-24 13:01:13 +0200

    Add SEOMetadata interface and improve BaseHead props
    Created a new SEOMetadata interface to define SEO-related properties. Refactored the BaseHead component to extend SEOMetadata, simplifying the props structure. Improved permalink handling by using the URL constructor.

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface SEOMetadata {
+    title: string;
+    description: string;
+    permalink: string;
+    ogImage?: string;
+    articlePublishedTime?: string;
+    articleModifiedTime?: string;
+}
