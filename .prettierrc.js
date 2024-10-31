1810d6477dee2454186be10f088a524435b7ffe9
Václav Vančura <commit@vancura.dev>
2024-10-31 12:27:51 +0100

    Add prettier-plugin-jsdoc and update .prettierrc.js configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,11 @@ let organizeImportsPlugin = require.resolve('prettier-plugin-organize-imports');
 let organizeAttributesPlugin = require.resolve('prettier-plugin-organize-attributes');
 let astroOrganizeImportsPlugin = require.resolve('prettier-plugin-astro-organize-imports');
 let cssOrderPlugin = require.resolve('prettier-plugin-css-order');
+let tailwindPlugin = require.resolve('prettier-plugin-tailwindcss');
+let jsdocPlugin = require.resolve('prettier-plugin-jsdoc');
 
 module.exports = {
-    plugins: [astroPlugin, organizeImportsPlugin, organizeAttributesPlugin, astroOrganizeImportsPlugin, cssOrderPlugin],
+    plugins: [astroPlugin, organizeImportsPlugin, organizeAttributesPlugin, astroOrganizeImportsPlugin, cssOrderPlugin, tailwindPlugin, jsdocPlugin],
     tabWidth: 4,
     useTabs: false,
     printWidth: 1000,
@@ -31,6 +33,48 @@ module.exports = {
             files: '*.mdx',
             options: {
                 parser: 'mdx'
+            }
+        },
+        {
+            files: '*.json',
+            options: {
+                parser: 'json'
+            }
+        },
+        {
+            files: '*.yml',
+            options: {
+                parser: 'yaml'
+            }
+        },
+        {
+            files: '*.yaml',
+            options: {
+                parser: 'yaml'
+            }
+        },
+        {
+            files: '*.md',
+            options: {
+                parser: 'markdown'
+            }
+        },
+        {
+            files: '*.js',
+            options: {
+                parser: 'babel'
+            }
+        },
+        {
+            files: '*.ts',
+            options: {
+                parser: 'typescript'
+            }
+        },
+        {
+            files: '*.tsx',
+            options: {
+                parser: 'typescript'
             }
         }
     ]
