586428711712be935570801d38ebad53960cbff7
Václav Vančura <commit@vancura.dev>
2024-11-06 10:32:50 +0100

    Remove `SCROLL_OFFSET` from `COMPONENT_CONFIG` and `TOCConfig`
    The `SCROLL_OFFSET` field has been removed from both the `COMPONENT_CONFIG` object in `components.ts` and the `TOCConfig` interface in `toc.ts`. This cleanup eliminates redundant configuration that was previously used to account for navbar height, streamlining the configuration setup.

--- src/config/components.ts
+++ src/config/components.ts
@@ -19,8 +19,7 @@ export const COMPONENT_CONFIG = {
     HEADING_LINKS: {
         SELECTOR: '.mdx-content h2, .mdx-content h3, .mdx-content h4',
         ID_REGEX: /[^a-z0-9]+/g,
-        TRIM_REGEX: /(^-|-$)/g,
-        SCROLL_OFFSET: 60 // offset to account for the navbar height.
+        TRIM_REGEX: /(^-|-$)/g
     },
 
     // MDX Wrapper constants.
@@ -41,8 +40,7 @@ export const COMPONENT_CONFIG = {
         PROGRESS_BAR_DEBOUNCE: 16,
         SCROLL_THRESHOLD: 50,
         MOUSE_THRESHOLD: 400,
-        OBSERVER_MARGIN: '0px 0px -80% 0px',
-        SCROLL_OFFSET: 60
+        OBSERVER_MARGIN: '0px 0px -80% 0px'
     } satisfies TOCConfig
 } as const;
 
